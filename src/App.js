841604c4b808fdbf6fd55c443f111c8b4a1d47fb
Carlos Valencia Henao <rasfreed@gmail.com>
2021-07-25 17:35:18 -0500

    Se agregaron rutas para usar el react-router-dom, se configura el router, switch y route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import React from 'react';
+import {
+  Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+import { history } from './utils/history'
+
 function App() {
   return (
-    
+    <Router history={history}>
+      <Switch>
+        <Route exact path="/">
+
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
